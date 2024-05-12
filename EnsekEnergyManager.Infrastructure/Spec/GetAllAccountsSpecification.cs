2fbe387e92411e8284b515cf4ca7c2d30d5acfc5
Dan Foster <dwfoster45@btinternet.com>
2024-05-12 20:14:58 +0100

    #-8 - cleanup

---
+++ EnsekEnergyManager.Infrastructure/Spec/GetAllAccountsSpecification.cs
@@ -0,0 +1,18 @@
+﻿using Ardalis.Specification;
+using EnsekEnergyManager.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnsekEnergyManager.Infrastructure.Spec
+{
+    //public class GetAllAccountsSpecification : Specification<Account>
+    //{
+    //    public override async Task<IEnumerable<Account>> ExecuteAsync(IQueryable<Account> query, CancellationToken cancellationToken = default)
+    //    {
+    //        return await Task.FromResult(query);
+    //    }
+    //}
+}
